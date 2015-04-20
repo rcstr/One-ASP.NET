5c9f80b7a640fc0355300bc7f2363172e88ececf
Rommel Castro <rommelxcastro@gmail.com>
2015-04-20 05:32:48 -0600

    ComputerOrder Model

---
+++ ComputerOrderForm/ComputerOrderForm/Models/ComputerOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComputerOrderForm.Models
+{
+    public class ComputerOrder
+    {
+        public int OrderId { get; set; }
+        public string OrderName { get; set; }
+        public string Customer { get; set; }
+        public string CustomerEmail { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int PartNumber { get; set; }
+        public bool Rush { get; set; }
+    }
+}
